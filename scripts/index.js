2bd43b9a43419ad26b4d5e78be2964fa9905ccff
sir21 <sasitha.iresha@gmail.com>
2018-10-11 07:32:48 +0530

    advance snake

--- scripts/index.js
+++ scripts/index.js
@@ -13,14 +13,11 @@ let snake = [
 //Getting the elemesnts from canvas
 var area = document.getElementById("area");
 var ctx = area.getContext("2d");
+var dx = 10;
 
 drawSnake();
-
-//Fill the canvas
-ctx.fillStyle = CANVAS_BACKGROUND_COLOUR;
-ctx.strokestyle = CANVAS_BORDER_COLOUR;
-ctx.fillRect(0, 0, area.width, area.height);
-ctx.strokeRect(0, 0, area.width, area.height);
+advanceSnake();
+drawSnake();
 
 //Dra snake function
 function drawSnakePart(snakePart) {
@@ -28,9 +25,29 @@ function drawSnakePart(snakePart) {
     ctx.strokestyle = 'darkgreen';
     ctx.fillRect(snakePart.x, snakePart.y, 10, 10);
     ctx.strokeRect(snakePart.x, snakePart.y, 10, 10);
-    console.log('Drawing snake is done');
 }
 
 function drawSnake() {
+    console.log('draw snake');
+    drawBackground();
+    console.log('snake: ' + JSON.stringify(snake));
     snake.forEach(drawSnakePart);
+    snake.forEach(advanceSnake);
+}
+
+//Fill the canvas
+function drawBackground() {
+    // ctx.globalCompositeOperation = 'destination-over';
+    ctx.fillStyle = CANVAS_BACKGROUND_COLOUR;
+    ctx.strokestyle = CANVAS_BORDER_COLOUR;
+    ctx.fillRect(0, 0, area.width, area.height);
+    ctx.strokeRect(0, 0, area.width, area.height);
+}
+
+function advanceSnake() {
+    for (i = 1; i < snake.length; i++) {
+        console.lof(snake[i-1].x, snake[i-1].y)
+        // snake[i] = { x = snake[i - 1].x, y = snake[i - 1].y };
+    }
+    snake[0] = { x = x+ x.dx, y = y };
 }
